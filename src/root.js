66db727dfa45f51f62ab0a0a581b34234c3b2b31
Sergiy Yemelyanov <sergey.yemelyanov@swanlogic.com>
2020-07-14 18:05:28 +0300

    Add splash screen to the app.

--- src/root.js
+++ src/root.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar, Platform } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import Routes from './routes/routes';
 
-const appRoot = (props) => (
-    <View style={{ flex:1 }}>
-        <StatusBar
-            barStyle={ `${Platform.OS === 'ios' ? 'dark-content' : 'light-content' }` }
-        />
+const appRoot = (props) => {
+    useEffect(() => {
+        SplashScreen.hide();
+    });
+    return (
+        <View style={{ flex:1 }}>
+            <StatusBar
+                barStyle={ `${Platform.OS === 'ios' ? 'dark-content' : 'light-content' }` }
+            />
             <Routes/>
-    </View>
-);
+        </View>
+    );
+};
 
 export default appRoot;
